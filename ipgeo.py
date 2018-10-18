0749f4f4374bbe3303dcf552704e237780b17640
c0d3ninjapy <43689157+c0d3ninjapy@users.noreply.github.com>
2018-10-17 21:50:27 -0500

    Create ipgeo.py

---
+++ ipgeo.py
@@ -0,0 +1,57 @@
+import pygeoip, socket
+
+# IP GEO v1.0 by c0d3ninja
+# Instagram: c0d3ninja
+# Discord: discord.gg/JV6pu5q
+
+gip = pygeoip.GeoIP('GeoIP.dat')
+gipcity = pygeoip.GeoIP('GeoLiteCity.dat')
+
+ascii = """
+
+$$$$$$\ $$$$$$$\         $$$$$$\  $$$$$$$$\  $$$$$$\  
+\_$$  _|$$  __$$\       $$  __$$\ $$  _____|$$  __$$\ 
+  $$ |  $$ |  $$ |      $$ /  \__|$$ |      $$ /  $$ |
+  $$ |  $$$$$$$  |      $$ |$$$$\ $$$$$\    $$ |  $$ |
+  $$ |  $$  ____/       $$ |\_$$ |$$  __|   $$ |  $$ |
+  $$ |  $$ |            $$ |  $$ |$$ |      $$ |  $$ |
+$$$$$$\ $$ |            \$$$$$$  |$$$$$$$$\  $$$$$$  |
+\______|\__|             \______/ \________| \______/ 
+                                                      
+"""
+
+print ascii + "\n"
+print "Author: c0d3ninja" + "\n"
+
+
+host = "microsoft.com"
+ip = socket.gethostbyname(host)
+
+print "Website: " + host
+print "IP: " + ip
+
+print "----------------------"
+
+print "Country: " + gip.country_name_by_addr(ip)
+print "Time Zone: " + gipcity.time_zone_by_addr(ip)
+
+record = gipcity.record_by_addr(ip)
+
+continent = record["continent"]
+areacode = record["area_code"]
+countrycode = record["country_code3"]
+postalcode = record["postal_code"]
+latitude = record["latitude"]
+longitude = record["longitude"]
+city = record["city"]
+metrocode = record["metro_code"]
+print "City: " + city
+print "Continent: " + continent
+print "Country Code: " + countrycode
+print "Postal Code: " + postalcode
+print "Area Code: " + str(areacode)
+print "Metro code: " + str(metrocode)
+print "Latitude: " + str(latitude)
+print "Longitude: " + str(longitude)
+
+print "----------------------"
